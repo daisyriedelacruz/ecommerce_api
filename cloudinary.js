19ca1bffb64d4d7b45b368c2bff0a9fec4d79c5a
daisyrie.delacruz <daisyrie.delacruz@tuitt.com>
2026-07-15 01:07:24 +0800

    Add cloudinary

--- cloudinary.js
+++ cloudinary.js
@@ -2,18 +2,16 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-// Configure Cloudinary with credentials from your .env file
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Define where and how to save uploaded files on the cloud
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "crafty_club_bouquets", // Folder name inside your Cloudinary account
+    folder: "crafty_club_bouquets",
     allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
